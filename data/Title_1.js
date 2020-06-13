f8d4da02eff073669e19f3c2364ab7dace9b256b
ajay1240 <66860958+ajay1240@users.noreply.github.com>
2020-06-13 19:14:25 +0545

    Add files via upload

---
+++ data/Title_1.js
@@ -0,0 +1,10 @@
+var json_Title_1 = {
+"type": "FeatureCollection",
+"name": "Title_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Designer": "Designed By: Ajay Yadav" }, "geometry": { "type": "Point", "coordinates": [ 80.21440778974852, 26.588883586901769 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Designer": "COVID-19 Outbreak in Nepal" }, "geometry": { "type": "Point", "coordinates": [ 82.965112294044104, 30.564287024923207 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Designer": "Updated : 13-June-2020; 1 PM" }, "geometry": { "type": "Point", "coordinates": [ 83.730868661125001, 25.756036360675083 ] } }
+]
+}
